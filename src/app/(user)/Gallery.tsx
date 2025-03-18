b97d44c51a917b413ae7bdf4ae4067d75beee460
Bao Nguyen <rocketvuong@gmail.com>
2025-03-18 17:20:29 -0400

    Gallery Loading feature implemented without images

--- src/app/(user)/Gallery.tsx
+++ src/app/(user)/Gallery.tsx
@@ -1,6 +1,7 @@
 import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView, FlatList, Platform } from "react-native"
 import { StatusBar } from "expo-status-bar"
 import { router } from "expo-router"
+import * as SQLite from 'expo-sqlite';
 
 // Define an interface for the meal item
 interface MealItem {
@@ -11,6 +12,17 @@ interface MealItem {
 // This is just a placeholder for now, you can change this however you see fit
 
 export default function MealGalleryScreen() {
+  const db = SQLite.useSQLiteContext();
+  console.log("Gallery Database Loading...");
+  var meals2 : MealItem[] = []
+
+  const result = db.getAllSync(`SELECT * FROM meals`);
+  let row: any
+  for (row of result){
+    console.log(row.id,row.name,row.description)
+    meals2.push({ id: row.id, name: row.name, image: "placeholder" })
+  }
+  
   // Sample data - replace with your actual meal data
   const meals: MealItem[] = [
     { id: "1", name: "Chicken Pasta", image: "https://placeholder.com/300" },
@@ -54,7 +66,7 @@ export default function MealGalleryScreen() {
 
       {/* Gallery Grid */}
       <FlatList
-        data={meals}
+        data={meals2}
         renderItem={renderMealItem}
         keyExtractor={(item) => item.id}
         numColumns={2}
