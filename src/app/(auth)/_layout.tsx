ccceaba0a462b2df34e126a335b78352d5b4a1ce
Julian <jramirez28@ggc.edu>
2025-04-19 17:18:14 -0400

    updated maindashboard title adjustment for ios updated _layout(user) bottom nav to be a little bit higher added license block for all the following presented

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,7 +1,22 @@
+/*
+ * This file is part of Meal Manager.
+ *
+ * Meal Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Meal Manager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Meal Manager. If not, see <http://www.gnu.org/licenses/>.
+ */
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import { Tabs } from 'expo-router';
 
 import Colors from '@/src/constants/Colors';
 import { useColorScheme } from '@/src/components/useColorScheme';
--- src/app/(user)/CreateMeal.tsx
+++ src/app/(user)/CreateMeal.tsx
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Meal Manager.
+ *
+ * Meal Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Meal Manager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Meal Manager. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import React, { useState, useEffect } from "react";
 import {
   View,
@@ -14,14 +31,9 @@ import {
   FlatList,
   Alert,
 } from "react-native";
-// import { Image } from 'expo-image';
 import { StatusBar } from "expo-status-bar";
 import { router, useLocalSearchParams } from "expo-router";
 import Colors from "@/src/constants/Colors";
-// Comment out Firebase imports for testing
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { FIREBASE_DB } from "@/src/lib/firebaseConfig";
-import { doc, updateDoc, getDoc } from "firebase/firestore";
 import { AutoId } from "@/src/lib/util";
 import { useMeals } from "@/src/context/MealsContext";
 import { Ingredient, Meal } from "@/src/types";
